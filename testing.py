206898fb6351e46b85f75bceb24fc44175048dae
Zak Xu <xzx624@gmail.com>
2021-04-17 00:52:11 -0400

    update april 17

--- testing.py
+++ testing.py
@@ -8,3 +8,5 @@ print("hello world!")
 # git push -u origin master
 
 
+# https://github.com/mandyyyyy123/AlgoTrading/blob/master/testing.py
+# https://medium.com/mindorks/what-is-git-commit-push-pull-log-aliases-fetch-config-clone-56bc52a3601c
